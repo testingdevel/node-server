8b55b146a4d7d0d1eb0520d04c284af73170c72e
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-01-17 20:15:49 -0500

    Add express server

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ exports.get = function(event, context, callback) {
 
   callback(null, result);
 };
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World from Express!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
