22ad5aec220f515f3eaa93fb67558572c26201f6
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-01-17 20:26:43 -0500

    Remove old js response

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-'use strict';
-
-var fs = require('fs');
-var path = require('path');
-
-exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
-  var result = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: {'content-type': 'text/html'}
-  };
-
-  callback(null, result);
-};
+// 'use strict';
+//
+// var fs = require('fs');
+// var path = require('path');
+//
+// exports.get = function(event, context, callback) {
+//   var contents = fs.readFileSync(`public${path.sep}index.html`);
+//   var result = {
+//     statusCode: 200,
+//     body: contents.toString(),
+//     headers: {'content-type': 'text/html'}
+//   };
+//
+//   callback(null, result);
+// };
 
 
 const express = require('express')
